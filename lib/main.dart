60c16d73d39e81cf2c839795ff7bf7a81b7ce018
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-07 12:59:51 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,74 +29,116 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
   var ftController = TextEditingController();
   var inController = TextEditingController();
 
+  var result = "";
+  var bgColor  = Colors.indigo.shade200;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("BMI"),
       ),
-      body: Center(
-        child: Container(
-          width:300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Text(
-                'BMI',
-                style: TextStyle(
-                  fontSize: 34,
-                  fontWeight: FontWeight.w700,
+      body: Container(
+        color: bgColor,
+        child: Center(
+          child: Container(
+            width:300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text(
+                  'BMI',
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              SizedBox(height: 21,),
-              TextField(
-                controller: wtController,
-                decoration: InputDecoration(
-                  label: Text('Enter your Weight in kgs)'),
-                  prefixIcon: Icon(Icons.line_weight)
+                SizedBox(height: 21,),
+                TextField(
+                  controller: wtController,
+                  decoration: InputDecoration(
+                    label: Text('Enter your Weight in kgs)'),
+                    prefixIcon: Icon(Icons.line_weight)
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
-              ),
 
-              SizedBox(height: 11,),
-              TextField(
-                controller: inController,
-                decoration: InputDecoration(
-                  label: Text('Enter your Height(in Feet)'),
-                  prefixIcon: Icon(Icons.height),
+                SizedBox(height: 11,),
+                TextField(
+                  controller: inController,
+                  decoration: InputDecoration(
+                    label: Text('Enter your Height(in Feet)'),
+                    prefixIcon: Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
-              ),
 
-              SizedBox(height: 11,),
+                SizedBox(height: 11,),
 
 
-              TextField(
-                controller: ftController,
-                decoration: InputDecoration(
-                  label: Text('Enter your Height (in inch)'),
-                  prefixIcon: Icon(Icons.height),
+                TextField(
+                  controller: ftController,
+                  decoration: InputDecoration(
+                    label: Text('Enter your Height (in inch)'),
+                    prefixIcon: Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
                 ),
-                keyboardType: TextInputType.number,
-              ),
 
-                SizedBox(height: 16,),
+                  SizedBox(height: 16,),
 
-                ElevatedButton(onPressed: (){
+                  ElevatedButton(onPressed: (){
 
-                  var wt = wtController.text.toString();
-                  var ft = ftController.text.toString();
-                  var inch = inController.text.toString();
+                    var wt = wtController.text.toString();
+                    var ft = ftController.text.toString();
+                    var inch = inController.text.toString();
 
-                  if(wt!="" && ft!="" && inch!="") {
+                    if(wt!="" && ft!="" && inch!="") {
 
-                  } else{
 
-                  }
-                }, child: Text ('Calculate')),
+                      var iWt = int.parse(wt);
+                      var iFt = int.parse(ft);
+                      var iInch = int.parse(inch);
 
-                Text('')
-            ],
+                      var tInch =(iFt*12) + iInch;
+
+                      var tCm =tInch*2.54;
+
+                      var tM = tCm/100;
+
+                      var bmi = iWt/(tM*tM);
+
+                      var msg = "";
+                      if(bmi>25){
+                        msg = "You're OverWeight!!";
+                        bgColor = Colors.orange.shade200;
+
+                      } else if(bmi<18){
+                        msg = "You're UnderWeight!!";
+                        bgColor = Colors.red.shade200;
+                      } else {
+
+                      }
+                      setState(() {
+                        msg = "You're Healthy!!";
+                        bgColor = Colors.green.shade200;
+                        result = "Your BMI is:${bmi.toStringAsFixed(4)}";
+                      });
+
+
+                    } else{
+                      setState(() {
+                        result = "Please fill all the required blanks!! ";
+                      });
+
+                    }
+                  }, child: Text ('Calculate')),
+
+                  SizedBox(height: 11,),
+
+                  Text(result, style: TextStyle(fontSize: 19),)
+              ],
+            ),
           ),
         ),
       ),
